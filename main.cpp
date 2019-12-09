5d46c461224b92b1245a36ecd271a9e796949df9
Your Name <destiny>
2019-12-09 22:59:44 +0800

    new Log

--- main.cpp
+++ main.cpp
@@ -1,27 +1,67 @@
-#include "CatLog.h"
+#include "Log.h"
 #include <iostream>
 #include <thread>
 int main()
 {
-    CatLog::Log::Instance();
+
+    Log* Logs = Log::Instance();
+    Log::InIt_Consumer_Thread(Log::WRITE_FILE);
+    Log::InIt_Consumer_Thread(Log::WRITE_FILE, "demo");
+    Log::InIt_Consumer_Thread(Log::WRITE_FILE, "cat");
+    Log::InIt_Consumer_Thread(Log::WRITE_FILE, "bbq");
+    Log::InIt_Consumer_Thread(Log::WRITE_FILE, "bbc");
     std::thread th([](){
         for(int i = 0; i < 1000; i++)
         {
-            CATLOG(CatLog::Log::LEVEL_DEBUG, "Thread this log %d %s", i, "...");
+            Log* Logs = Log::Instance();
+            Logs->AddLog_Detailed("main", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this thread log %d %s", i, "...");
         }
     });
+    
+    std::thread thdemo([](){
+        for(int i = 0; i < 1000; i++)
+        {
+            Log* Logs = Log::Instance();
+            Logs->AddLog_Detailed("demo", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this thread log %d %s", i, "...");
+        }
+    });
+    
+    std::thread thcat([](){
+        for(int i = 0; i < 1000; i++)
+        {
+            Log* Logs = Log::Instance();
+            Logs->AddLog_Detailed("cat", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this thread log %d %s", i, "...");
+        }
+    });
+
+    std::thread thbbq([](){
+        for(int i = 0; i < 1000; i++)
+        {
+            Log* Logs = Log::Instance();
+            Logs->AddLog_Detailed("bbq", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this thread log %d %s", i, "...");
+        }
+    });
+
+    std::thread thbbc([](){
+        for(int i = 0; i < 1000; i++)
+        {
+            Log* Logs = Log::Instance();
+            Logs->AddLog_Detailed("bbc", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this thread log %d %s", i, "...");
+        }
+    });
+    
     for(int i = 0; i < 1000; i++)
     {
-        CATLOG_DETAILED(CatLog::Log::LEVEL_DEBUG, "CATLOG this log %d %s", i, "...");
+        Logs->AddLog_Detailed("main", Log::LEVEL_DEBUG,  __FILE__, __FUNCTION__, __LINE__, "CATLOG this log %d %s", i, "...");
     }
     
-    th.join();
 
-    CatLog::Log::SetLog_Print(true);
-    for(int i = 0; i < 1000; i++)
-    {
-        CATLOG_DETAILED(CatLog::Log::LEVEL_DEBUG, "PRINTF this log %d %s", i, "...");
-    }
-    CatLog::Log::Delete();
+    th.join();
+    thdemo.join();
+    thcat.join();
+    thbbq.join();
+    thbbc.join();
+    Log::Delete();
+
     return 0;
 }
